5161b5e931b709fdea0fa45c8dae40dee8af23a4
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-17 20:19:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -23,17 +23,9 @@ class Song
     @artist = artist
   end
 
-  # def self.reset_all
-  #   @@songs.clear
-  # end
-
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def self.all
     @@songs
